bb7f32cff38a0eb212fa6254c434c5f59e3b9d31
RamonCato <ramoncato1@gmail.com>
2026-09-14 11:50:07 -0500

    Component Project Code

---
+++ SecondComponent.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class SecondComponent : MonoBehaviour
+{
+    public Color SpriteColor { get; set; } = Color.red;
+
+    public Color customColor = Color.red;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
